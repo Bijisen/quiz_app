3d5ea39a1d3280e67c9290baeb58f3b3530c4a55
omr-k <inoue66618@gmail.com>
2019-01-16 16:37:40 +0900

    omr-k QuestionActivity.kt の更新

--- app/src/main/java/com/example/android/sample/quizapp/QuestionActivity.kt
+++ app/src/main/java/com/example/android/sample/quizapp/QuestionActivity.kt
@@ -11,20 +11,20 @@ class QuestionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
-        var undobtn = findViewById<Button>(R.id.undoButton)
+        val undobtn = findViewById<Button>(R.id.undoButton)
         undobtn.setOnClickListener {
             finish()
         }
 
-        var titlebtn = findViewById<Button>(R.id.titleButton)
+        val titlebtn = findViewById<Button>(R.id.titleButton)
         titlebtn.setOnClickListener {
             intent = Intent(this, TitleActivity::class.java)
             startActivity(intent)
         }
 
-        var nextbtn = findViewById<Button>(R.id.nextButton)
+        val nextbtn = findViewById<Button>(R.id.nextButton)
 
-        var resultbtn = findViewById<Button>(R.id.resultButton)
+        val resultbtn = findViewById<Button>(R.id.resultButton)
         resultbtn.setOnClickListener {
             intent = Intent(this, ResultActivity::class.java)
             startActivity(intent)
