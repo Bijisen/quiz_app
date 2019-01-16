a9b8a5a6f9111ffae486195b489b17f1bd351a35
Mr.k <a@a.com>
2019-01-16 14:49:10 +0900

    2019 1/16 (水) 足立 　ファイルの更新

--- app/src/main/java/com/example/android/sample/quizapp/QuestionNumberActivity.kt
+++ app/src/main/java/com/example/android/sample/quizapp/QuestionNumberActivity.kt
@@ -1,12 +1,29 @@
 package com.example.android.sample.quizapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_anime_title.*
+import kotlinx.android.synthetic.main.activity_question.*
 
 class QuestionNumberActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_number)
+
+        val nextButton = findViewById<Button>(R.id.nextButton)
+        val backButton = findViewById<Button>(R.id.backButton)
+
+        nextButton.setOnClickListener(){
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+        }
+
+        backButton.setOnClickListener(){
+            val intent = Intent(this, GenreActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
