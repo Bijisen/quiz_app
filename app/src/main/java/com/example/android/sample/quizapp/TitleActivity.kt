9d8957f4779bb161bab2a3a5f885ded514f76624
InoueTakahiro <inoue66618@gmail.com>
2019-01-16 15:19:29 +0900

    2019/1/16 T-Virus
    ・マニフェストファイルにActivityリストを追加
    ・タイトル＝＞アニメ選択画面への遷移 追加

--- app/src/main/java/com/example/android/sample/quizapp/TitleActivity.kt
+++ app/src/main/java/com/example/android/sample/quizapp/TitleActivity.kt
@@ -1,12 +1,21 @@
 package com.example.android.sample.quizapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TitleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
+
+        val startBtn = findViewById<Button>(R.id.startBtn)
+
+        startBtn.setOnClickListener {
+            val intent = Intent(this, AnimeTitleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
