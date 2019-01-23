850b9d9f0d646dc40f71d75720cbe25ff6448de6
ファミZin <a@a.com>
2019-01-23 12:13:22 +0900

    2019/01/23 レイアウトの編集

--- app/src/main/java/com/example/android/sample/quizapp/ResultActivity.kt
+++ app/src/main/java/com/example/android/sample/quizapp/ResultActivity.kt
@@ -1,12 +1,22 @@
 package com.example.android.sample.quizapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val titlebtn = findViewById<Button>(R.id.titleButton)
+        titlebtn.setOnClickListener {
+            intent = Intent(this, TitleActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
