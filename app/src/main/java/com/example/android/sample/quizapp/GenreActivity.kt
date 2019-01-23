2b37d2f1ca9c9ffb672aee842696bf79b1bcad87
kazuta <a@a.com>
2019-01-23 12:44:22 +0900

    2019/1/23 kazuta 画面遷移内容を更新

--- app/src/main/java/com/example/android/sample/quizapp/GenreActivity.kt
+++ app/src/main/java/com/example/android/sample/quizapp/GenreActivity.kt
@@ -1,12 +1,36 @@
 package com.example.android.sample.quizapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GenreActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_genre)
+
+     val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
+
+     val button5 = findViewById<Button>(R.id.button5)
+     val titleButton = findViewById<Button>(R.id.titleButton)
+     val button6 = findViewById<Button>(R.id.button6)
+
+     button5.setOnClickListener{
+         finish()
+     }
+
+     titleButton.setOnClickListener() {
+         val intent = Intent(this, TitleActivity::class.java)
+         startActivity(intent)
+     }
+
+     button6.setOnClickListener(){
+         val intent = Intent
+     }
+
     }
+
 }
